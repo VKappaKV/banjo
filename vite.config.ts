3f310737b13ecd844e8460508daa996447549a4f
VKappaKV <jacopo851@gmail.com>
2026-05-23 22:04:04 +0200

    tailwind config

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,6 @@
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [svelte()],
-  server: {
-    port: 9000,
-  },
-});
+export default defineConfig({ plugins: [tailwindcss(), svelte()], server: { port: 9000 } });
