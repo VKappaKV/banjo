34e479f97b67ab0de14a4ad2987897904574bb53
VKappaKV <jacopo851@gmail.com>
2026-05-23 22:21:06 +0200

    (●'◡'●) shadcn is here bbs

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,11 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import { svelte } from "@sveltejs/vite-plugin-svelte";
+import path from "path";
 
 // https://vite.dev/config/
-export default defineConfig({ plugins: [tailwindcss(), svelte()], server: { port: 9000 } });
+export default defineConfig({
+  plugins: [tailwindcss(), svelte()],
+  server: { port: 9000 },
+  resolve: { alias: { $lib: path.resolve("./src/lib") } }
+});
